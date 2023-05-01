2e09305465778f21b8a630083285d902cadc44b2
Alexey Pakhomov <pakhomovs.mail@yandex.ru>
2023-05-01 14:00:58 +0300

    feat: logger fix: mongodb

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 const { limiter } = require('./utils/config');
 const routes = require('./routes');
 const error = require('./middlewares/error');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 const { PORT = 3000 } = process.env;
@@ -14,10 +15,13 @@ const { PORT = 3000 } = process.env;
 app.use(helmet());
 app.use(limiter); // Ограничение распространяется на все окна
 app.use(bodyParser.json());
+app.use(requestLogger); // подключаем логгер запросов до всех роутов
 
 app.use(routes);
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+app.use(errorLogger); // подключаем логгер ошибок после обработчиков роутов и до обработчиков ошибок
+
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(errors()); // обработчик ошибок celebrate
 
